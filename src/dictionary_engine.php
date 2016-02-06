0474850165be8b01345a8e9f4e29e01fb4a61902
MAC <mac@MACs-MacBook-Pro.local>
2016-02-06 01:01:37 +0100

    completed ranking system (#113030979),also completed Cruz(#113030555) and  organised my work

--- src/dictionary_engine.php
+++ src/dictionary_engine.php
@@ -7,46 +7,56 @@ class Crud implement dictionary
 {
      public function add ($word, $description, $sample_sentence) 
      {
-         return array_push ($data, [$this-> word => $this -> description $this ->sample_sentence])
+         return array_push ($data, [$this-> word => $this -> description $this -> sample_sentence])
      }   
 
-     public function retrieve ($word, $description, $sample_sentence)
+     public function retrieve ($word)
      {
-         foreach ($data as  $key =>$val) 
+         foreach ($data as $key => $value) 
          {
-
-            
-             /*if $data["slang"] == $word 
+             foreach ($data[$key] as $k => $v) 
              {
-                 return $data["slang"] . ":" . $data["description"] . "usage:" . $data["sample-sentence"];
-             } 
-             else 
-             {
-                throw new exception("word not found in the dictioanry");
-             }  */ 
-         }    
+                 if ($key === $word)
+                 {
+                     return $key . ":" . $k . "Usage: " . $v 
+                 }
+                 /*else
+                 {
+                     throw exeption("word not found in the dictionary")
+                 } */ 
+             }  
+         }
+   }
      }
 
-     public function update ($old_value, $new_value,)
+     public function update ($word, $description, $sample_sentence)
      {
-         foreach ($data as $key => $value)
+         foreach ($data as $key => $value) 
          {
-             if $data["slang"] == $old_value
+             foreach ($data[$key] as $k => $v) 
              {
-                 return $data["slang"]   
-             } 
-         }   
-         return 
+                 if ($key === $word)
+                 {
+                     $k["v"][0] = $description
+                     $k["v"][1] = $sample_sentence
+                 }
+                 /*else
+                 {
+                     throw exeption("word not found in the dictionary")
+                 } */ 
+             }  
+         }
      }
 
      public function delete ($word)
      {
-         foreach ($data as $key => value)
+         foreach ($data as $key => $value)
          {
-             if $data["slang"] == "$word"
-             return unset ($data["slang"])   
-            
-         }
-         return    
+             if ($data == $word)
+             {   
+             return unset ($data);
+             }   
+         }   
+   
      }
 }
